065c22fa3d36a22b9ef9c926e1d88b4d668265f1
kenwekesa <kenwek1994@gmail.com>
2023-06-16 23:40:32 +0300

    Initia android commit

---
+++ app/src/main/java/com/ack/ststephenskayo/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.ack.ststephenskayo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ack.ststephenskayo.ui.login.LoginActivity
+import androidx.cardview.widget.CardView as CardView1
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setTitle("")
+
+        val guest_card = findViewById(R.id.guests_card) as CardView1
+        val members_card = findViewById(R.id.members_card) as CardView1
+
+        guest_card.setOnClickListener()
+        {
+            val intent = Intent(this, GuestActivity::class.java)
+            startActivity(intent)
+        }
+
+        members_card.setOnClickListener()
+        {
+            val intent = Intent(this, LoginActivity::class.java);
+            startActivity(intent)
+        }
+
+    }
+}
